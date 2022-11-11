4932c22c0f57e0f832d25e5e35151d6682221a9f
Miguel Wagner <wmiguel999@gmail.com>
2022-11-11 00:38:57 -0500

    Final

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -56,13 +56,13 @@ const ProductDetails = ({ product, products }) => {
             <h3>Quantity:</h3>
             <p className="quantity-desc">
               <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
-              <span className="num" onClick = "">{qty}</span>
+              <span className="num">{qty}</span>
               <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
             <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-            <button type="button" className="buy-now" onClick="">Buy Now</button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
       </div>
